4f27ab59621dc8fe11350cd19d508981792f8e0e
tanshiv2 <shivangi.tanwar4@gmail.com>
2020-07-02 22:15:26 +0530

    production ready

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/users')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 const router = new express.Router()
 
 router.post('/users', async (req,res) => {
@@ -103,7 +104,8 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res)=> {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.status(200).send()
 }, (error,req,res,next) => {
@@ -118,7 +120,7 @@ router.delete('/users/me/avatar', auth, async (req,res) => {
     res.status(400).send({error: 'Failed to delete image'})
 })
 
-router.get('/user/:id/avatar', async (req,res) => {
+router.get('/users/:id/avatar', async (req,res) => {
     try{
         const user = await User.findById(req.params.id)
 
@@ -126,7 +128,7 @@ router.get('/user/:id/avatar', async (req,res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
     } catch (e) {
         res.status(404).send()
